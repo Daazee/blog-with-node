a401af8ead397f726c3e560c3d57072d9de1edbe
Azeez Adebayo <azeez.adedayo@yahoo.com>
2022-02-04 20:18:50 +0100

    Modify index.js file to serve static file using res.sendfile(path to file) command

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = new express();
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
+
 app.listen(4000, (req, res)=> {
     console.log("App is listening on port 4000");
 })
