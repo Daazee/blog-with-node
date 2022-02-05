d59accbab2a3b4c8572c4b63da1ca23f4752ed70
Azeez Adebayo <azeez.adedayo@yahoo.com>
2022-02-05 21:10:13 +0100

    refactored all pages to use template engine

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 
-const expressEdge = require('express-edge');
 const express = require('express');
 const path = require('path');
+const {engine} = require('express-edge');
 
 const app = new express();
 
 app.use(express.static('public'));
-app.use(expressEdge);
+app.use(engine);
+
+app.set('views',`${__dirname}/views`);
 
 app.get('/', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    //res.sendFile(path.resolve(__dirname, 'pages/index.html')); //initial way without rendering
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 })
 
 app.get('/contact', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 })
 
 
 app.get('/post', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 })
 
 
